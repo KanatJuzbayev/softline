d5860d73b209f5b1127474f74df8baebf947a398
Kanat Juzbayev <kanat.juzbayev@gmail.com>
2023-03-01 15:09:51 +0600

    feat: add corporate-life section

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import { map } from "./js/map";
 import { dropdown } from "./js/dropdown";
+
+// import Swiper JS
+import Swiper from "swiper/bundle";
+
+// import Swiper styles
+import "../node_modules/swiper/swiper-bundle.min.css";
+
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+});
